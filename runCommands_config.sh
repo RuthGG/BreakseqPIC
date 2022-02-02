aec25d970f37fabe96d761574c46345e3bc2349b
ruthgg <g.g.ruth020695@gmail.com>
2022-02-02 10:41:42 +0100

    changes?

--- runCommands_config.sh
+++ runCommands_config.sh
@@ -21,7 +21,7 @@ OVERRIDE=0 # This is the first process number we want to run, e.g. 2 will not ru
 	# 1 download -> we can override it by just having a FASTQ variable, even with 0 value override
 		# If we want to override the download, we can set a directory with previous downloads
 		# It has to be the directory with individual directories
-		READ_FASTQS="/data/bioinfo/scratch/breakseq_fastqs/2021-09-20_v2.3.2_downloadReadsCheck"
+		# READ_FASTQS="/data/bioinfo/scratch/breakseq_fastqs/2021-09-20_v2.3.2_downloadReadsCheck"
 	# 2 breakseq
 		# BREAKSEQ_RESULTS="analysis/02_breakseq/2021-06-03/smallinvs_min_20" 
 	# 3 alignment analysis - it can be automatically assigned!
