11ba02ac7e5fa235c1ec235a2c46c40fb44908fb
Judit Camps Bruch <juditcampsbruch@MacBook-MacBook-Pro-de-Judit.local>
2022-02-18 14:58:50 +0100

    v6

--- runCommands_config.sh
+++ runCommands_config.sh
@@ -6,7 +6,7 @@
 # ALERT!!! YOU WON'T BE WARNED IF THIS ALREADY EXISTS!
 # =========================================================================== #
 
-NAME="v5.ancientGenomes"
+NAME="v6.ancientGenomes"
 
 # Manually set date, comment if we want it automatic
 DATE="2022-02-18"
