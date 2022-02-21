d3d9dce970634ef53c56d1451a33a74ccc3452ee
Judit Camps Bruch <juditcampsbruch@MacBook-MacBook-Pro-de-Judit.local>
2022-02-21 13:01:30 +0100

    error modification

--- runall.sh
+++ runall.sh
@@ -235,7 +235,7 @@ if [ "$COMMAND" == "download" ]; then
     FORMAT=$(cut -f3 $SAMPLE_LINE)
     if [[ $FORMAT == "FASTQ" ]]; then
       # We select for each sample the fastq files and save them as the selected regions for breakseq.
-      (grep /data/bioinfo/scratch/breakseq_fastqs/2022-02-07_ancientGenomes/${SAMPLE}/*.fastq) >> selected_regions.fastq
+      (grep /data/bioinfo/scratch/breakseq_fastqs/2022-02-21_ancientGenomes/${SAMPLE}/*.fastq) >> selected_regions.fastq
     else
       # Loop per inversion - align reads
       for REGION in $REGIONS; do
@@ -252,7 +252,7 @@ if [ "$COMMAND" == "download" ]; then
         # BAM_FILE="ftp://ftp.1000genomes.ebi.ac.uk/vol1/ftp/phase3/data/$SAMPLE/alignment/"$BAM_FILE
         # Already downloaded the files. Only one is in format BAM. Others are FASTQ files.
         # Locate the BAM file from the folder:
-        BAM_FILE= "/data/bioinfo/scratch/breakseq_fastq/2022-02-07_ancientGenomes/CHAGYRSKAYA8/chr22.bam"
+        BAM_FILE= "/data/bioinfo/scratch/breakseq_fastq/2022-02-21_ancientGenomes/CHAGYRSKAYA8/chr22.bam"
 
         # Download reads - Importante! Rango de extracción alrededor de la inversión! He puesto 20kb, pero quizá podría ser otro rango
         # Idealmente, habria que hacer una lista de coordenadas del alelo invertido para afinar esta region, pero todavia no existe
@@ -319,7 +319,7 @@ if [ "$COMMAND" == "download" ]; then
         # BAM_FILE="ftp://ftp.1000genomes.ebi.ac.uk/vol1/ftp/phase3/data/$SAMPLE/alignment/"$BAM_FILE
          # Already downloaded the files. Only one is in format BAM. Others are FASTQ files.
         # Locate the BAM file from the folder:
-        BAM_FILE= "/data/bioinfo/scratch/breakseq_fastq/2022-02-07_ancientGenomes/CHAGYRSKAYA8/chr22.bam"
+        BAM_FILE= "/data/bioinfo/scratch/breakseq_fastq/2022-02-21_ancientGenomes/CHAGYRSKAYA8/chr22.bam"
         # Select the unmapped reads from the bam file:
         BAM_FILE= $(samtools view -b -f 4 $BAM_FILE)
 
