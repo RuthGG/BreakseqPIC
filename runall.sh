a48575f3bc27b64fe721892d064cf4af37df7e4f
Judit Camps Bruch <juditcampsbruch@MacBook-MacBook-Pro-de-Judit.local>
2022-02-21 15:33:49 +0100

    modifications for error trial.

--- runall.sh
+++ runall.sh
@@ -205,6 +205,8 @@ if [ "$COMMAND" == "download" ]; then
   # Take samples
   SAMPLES=$SAMPLES_FILE
 
+  echo "$SAMPLES"
+
   # Take regions
   echo "Take regions"
   # How to create a file with all invs
@@ -212,7 +214,9 @@ if [ "$COMMAND" == "download" ]; then
 
   ## Loop per sample - align reads
   echo "Start looping samples"
+
   for SAMPLE_LINE in $SAMPLES; do
+    echo "sample line is $SAMPLE_LINE"
     SAMPLE=$(cut -f1 $SAMPLE_LINE)
     echo "################"
     echo "$SAMPLE"
@@ -252,7 +256,7 @@ if [ "$COMMAND" == "download" ]; then
         # BAM_FILE="ftp://ftp.1000genomes.ebi.ac.uk/vol1/ftp/phase3/data/$SAMPLE/alignment/"$BAM_FILE
         # Already downloaded the files. Only one is in format BAM. Others are FASTQ files.
         # Locate the BAM file from the folder:
-        BAM_FILE= "/data/bioinfo/scratch/breakseq_fastq/2022-02-21_ancientGenomes/CHAGYRSKAYA8/chr22.bam"
+        BAM_FILE="/data/bioinfo/scratch/breakseq_fastq/2022-02-21_ancientGenomes/CHAGYRSKAYA8/chr22.bam"
 
         # Download reads - Importante! Rango de extracción alrededor de la inversión! He puesto 20kb, pero quizá podría ser otro rango
         # Idealmente, habria que hacer una lista de coordenadas del alelo invertido para afinar esta region, pero todavia no existe
@@ -319,9 +323,9 @@ if [ "$COMMAND" == "download" ]; then
         # BAM_FILE="ftp://ftp.1000genomes.ebi.ac.uk/vol1/ftp/phase3/data/$SAMPLE/alignment/"$BAM_FILE
          # Already downloaded the files. Only one is in format BAM. Others are FASTQ files.
         # Locate the BAM file from the folder:
-        BAM_FILE= "/data/bioinfo/scratch/breakseq_fastq/2022-02-21_ancientGenomes/CHAGYRSKAYA8/chr22.bam"
+        BAM_FILE="/data/bioinfo/scratch/breakseq_fastq/2022-02-21_ancientGenomes/CHAGYRSKAYA8/chr22.bam"
         # Select the unmapped reads from the bam file:
-        BAM_FILE= $(samtools view -b -f 4 $BAM_FILE)
+        BAM_FILE=$(samtools view -b -f 4 $BAM_FILE)
 
         # This includes a loop to resume download in case it was interrupted
         i=0
