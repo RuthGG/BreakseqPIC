d456ba5b2ed6d4567fc7d8775d9392222352574a
Judit Camps Bruch <juditcampsbruch@MacBook-MacBook-Pro-de-Judit.local>
2022-02-22 10:03:25 +0100

    error correction trial

--- runall.sh
+++ runall.sh
@@ -265,7 +265,7 @@ if [ "$COMMAND" == "download" ]; then
         # BAM_FILE="ftp://ftp.1000genomes.ebi.ac.uk/vol1/ftp/phase3/data/$SAMPLE/alignment/"$BAM_FILE
         # Already downloaded the files. Only one is in format BAM. Others are FASTQ files.
         # Locate the BAM file from the folder:
-        BAM_FILE="/data/bioinfo/scratch/breakseq_fastq/2022-02-21_ancientGenomes/CHAGYRSKAYA8/chr22.bam"
+        BAM_FILE="/data/bioinfo/scratch/breakseq_fastq/2022-02-21_ancientGenomes/${SAMPLE}/*.bam"
 
         # Download reads - Importante! Rango de extracción alrededor de la inversión! He puesto 20kb, pero quizá podría ser otro rango
         # Idealmente, habria que hacer una lista de coordenadas del alelo invertido para afinar esta region, pero todavia no existe
@@ -332,7 +332,7 @@ if [ "$COMMAND" == "download" ]; then
         # BAM_FILE="ftp://ftp.1000genomes.ebi.ac.uk/vol1/ftp/phase3/data/$SAMPLE/alignment/"$BAM_FILE
          # Already downloaded the files. Only one is in format BAM. Others are FASTQ files.
         # Locate the BAM file from the folder:
-        BAM_FILE="/data/bioinfo/scratch/breakseq_fastq/2022-02-21_ancientGenomes/CHAGYRSKAYA8/chr22.bam"
+        BAM_FILE="/data/bioinfo/scratch/breakseq_fastq/2022-02-21_ancientGenomes/${SAMPLE}/*.bam"
         # Select the unmapped reads from the bam file:
         BAM_FILE=$(samtools view -b -f 4 $BAM_FILE)
 
