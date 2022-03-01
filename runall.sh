5c9567aad3e6882999be9cb5e59d4da0b9ba3429
Judit Camps Bruch <juditcampsbruch@MacBook-MacBook-Pro-de-Judit.local>
2022-03-01 13:26:40 +0100

    Error correction.

--- runall.sh
+++ runall.sh
@@ -225,6 +225,7 @@ if [ "$COMMAND" == "download" ]; then
     OTHER_FILE=$(grep $SAMPLE ${DATADIR}/samples_pathIndex.txt | cut -f3)
 
     MAIN_FORMAT=$(echo $MAIN_FILE | rev | cut -d "." -f1 | rev)
+    echo "$MAIN_FORMAT"
 
     # Make a folder for the sample
     mkdir -p ${OUTDIR}/${NAME}/${SAMPLE} 
